85765ab765646ccbc0d367423aecd8490871f98c
alexfbasa <47458685+alexfbasa@users.noreply.github.com>
2022-08-30 11:04:20 +0300

    change file

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/alexsimple/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/alexfbasa/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('alexsimple@gmail.com')
         }
